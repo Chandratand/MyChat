94de4b7e037c43cd2d960e1e20eb30fd1fd4c9d1
chandratand <Chandra.tand@gmail.com>
2021-10-14 15:14:46 +0700

    membuat method StartListeningIncomingConnection untuk menerima connection dari Client

--- MyChatSocketAsync/MyChatServer.cs
+++ MyChatSocketAsync/MyChatServer.cs
@@ -3,10 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
 
 namespace MyChatSocketAsync
 {
-    public class Class1
+    public class MyChatServer
     {
+        IPAddress mIP;
+        int mPort;
+        TcpListener mTCPListener;
+
+        public async void StartListeningForIncomingConnection(IPAddress ipaddr = null, int port = 23000)
+        {
+            if (ipaddr == null)
+            {
+                ipaddr = IPAddress.Any;
+            }
+            if (port <= 0)
+            {
+                port = 23000;
+            }
+
+            mIP = ipaddr;
+            mPort = port;
+
+            Console.WriteLine($"IP Address : {mIP} - Port : {mPort}");
+
+            mTCPListener = new TcpListener(mIP, mPort);
+            mTCPListener.Start();
+
+           var returnedByAccept =  await mTCPListener.AcceptTcpClientAsync();
+            Console.WriteLine("Client Berhasil Terhubung : " + returnedByAccept.ToString() );
+        }
+
     }
 }
